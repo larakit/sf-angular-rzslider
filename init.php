00a980dd94b3f8a24b1d1c1b135b9abc394b4573
Berdnikov Alexey <berdnikov_ay@taximaxim.ru>
2017-03-09 13:58:32 +0500

    fixed

---
+++ init.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Larakit.
+ * Link: http://github.com/larakit
+ * User: Alexey Berdnikov
+ * Date: 23.05.16
+ * Time: 10:02
+ */
+\Larakit\StaticFiles\Manager::package('larakit/sf-angular-rzslider')
+    ->setSourceDir('public')
+    ->ngModule('rzModule')
+    ->usePackage('larakit/sf-angular')
+    ->jsPackage('rzslider.min.js')
+    ->cssPackage('rzslider.min.css');
